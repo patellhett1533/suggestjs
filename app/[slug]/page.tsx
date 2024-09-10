fec749d5f3d333aca601b4418d294bff17075ed5
het patel <hppatidar12d@gmail.com>
2024-09-10 21:58:07 +0530

    update a readme section of detail page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
+import matter from 'gray-matter'
+import md from 'markdown-it'
 
 const getPackageData = async (name: string) => {
   const response = await fetch(`https://registry.npmjs.com/${name}`, {
@@ -13,13 +15,19 @@ const page = async ({ params }: { params: { slug: string } }) => {
   const packageData = await getPackageData(params.slug)
 
   if (!packageData) return <div></div>
+
+  const readme = matter(packageData.readme)
   return (
     <div>
       <div className="w-full p-12">
         <div className="w-full flex items-center justify-between border-b border-border pb-4">
           <h1 className="text-4xl font-semibold">{params.slug}</h1>
           <div>
-            <p>1.0.2 &nbsp;•&nbsp; 2 days ago &nbsp;•&nbsp; 267 downloads</p>
+            <p>
+              {packageData['dist-tags'].latest} &nbsp;•&nbsp;{' '}
+              {packageData.time.modified.split('T')[0]}
+              &nbsp;•&nbsp; 267 downloads
+            </p>
           </div>
         </div>
         <div className="w-full mt-8">
@@ -42,6 +50,15 @@ const page = async ({ params }: { params: { slug: string } }) => {
               <div
                 className="mt-4"
                 dangerouslySetInnerHTML={{ __html: packageData.description }}
+              ></div>
+            </div>
+            <div className="mt-12">
+              <h3 className="text-xl font-semibold">README File</h3>
+              <div
+                className="mt-4"
+                dangerouslySetInnerHTML={{
+                  __html: md().render(readme.content),
+                }}
               ></div>
             </div>
           </div>
