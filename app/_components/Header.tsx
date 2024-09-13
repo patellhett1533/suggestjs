70d8db57757b11362fc909552e7eb69a5fb83915
het patel <hppatidar12d@gmail.com>
2024-09-14 00:58:46 +0530

    update a header component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -5,7 +5,9 @@ import Image from 'next/image'
 const Header = () => {
   return (
     <div className="w-full px-12 h-24 flex items-center justify-between border-b border-border">
-      <p className="text-2xl font-semibold">Suggest js</p>
+      <Link href="/">
+        <Image src="/images/SuggestJS.png" width={150} height={50} alt="logo" />
+      </Link>
       <Link href="https://github.com/patellhett1533/suggestjs" target="_blank">
         <Image src="/images/github.svg" width={20} height={20} alt="github" />
       </Link>
